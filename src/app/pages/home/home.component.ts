2f03a659215a9ef01948df9710a98ca0facb0b29
DevJaredt <jaredtrujilloespinosa64@gmail.com>
2024-10-29 12:00:09 -0500

    Feat: style home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  links = [
+    {label: 'Home', anchor: 'home'},
+    {label: 'About', anchor: 'about'},
+    {label: 'Services', anchor: 'services'},
+    {label: 'Contact us', anchor: 'contact'}
+  ]
 }
